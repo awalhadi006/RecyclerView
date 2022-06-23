ee42835abe95ffc8d54986f4777c145dee18446b
awalhadi006 <awalhadi006@gmail.com>
2022-06-23 23:55:01 +0700

    RecyclerView APP update

--- app/src/main/java/com/awalhady/recycleview/MainActivity.kt
+++ app/src/main/java/com/awalhady/recycleview/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.awalhady.recycleview
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.ImageView
+import android.widget.TextClock
+import android.widget.TextView
 import com.awalhady.recycleview.adapter.AdapterTeamBola
 import com.awalhady.recycleview.databinding.ActivityMainBinding
 import com.awalhady.recycleview.model.Pemain
@@ -14,15 +19,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val listPemain = ArrayList<Pemain>()
-        listPemain.add(Pemain(nama = "Darman", R.drawable.darman, posisi = "Penjaga Gawang", tinggi = "2.00 m", tempatlahir = "Bree (Belgia)", tgllahir = "11 Mei 1992"))
-        listPemain.add(Pemain(nama = "Agim", R.drawable.agim, posisi = "Penyerang", tinggi = "1,85 m", tempatlahir = "Lyon (Perancis)", tgllahir = "19 Desember 1987"))
-        listPemain.add(Pemain(nama = "Arip", R.drawable.agim, posisi = "Belakang", tinggi = "1,74 m", tempatlahir = "Rio de Janeiro (Brasil)", tgllahir = "12 Mei 1988"))
-        listPemain.add(Pemain(nama = "Arya", R.drawable.agim, posisi = "Belakang", tinggi = "1,84 m", tempatlahir = "Camas (Sevilla)", tgllahir = "30 Maret 1986"))
-        listPemain.add(Pemain(nama = "Bani", R.drawable.bani, posisi = "Pelatih", tinggi = "1,85 m", tempatlahir = "Marseille (Prancis)", tgllahir = "23 Juni 1972"))
+        listPemain.add(Pemain(nama = "Thibaut Courtois", R.drawable.courtois, posisi = "Penjaga Gawang", tinggi = "2.00 m", tempatlahir = "Bree (Belgia)", tgllahir = "11 Mei 1992"))
+        listPemain.add(Pemain(nama = "Karim Benzema", R.drawable.benzema, posisi = "Penyerang", tinggi = "1,85 m", tempatlahir = "Lyon (Perancis)", tgllahir = "19 Desember 1987"))
+        listPemain.add(Pemain(nama = "Marcelo Vieira da Silva", R.drawable.marcello, posisi = "Belakang", tinggi = "1,74 m", tempatlahir = "Rio de Janeiro (Brasil)", tgllahir = "12 Mei 1988"))
+        listPemain.add(Pemain(nama = "Sergio Ramos García", R.drawable.ramos, posisi = "Belakang", tinggi = "1,84 m", tempatlahir = "Camas (Sevilla)", tgllahir = "30 Maret 1986"))
+        listPemain.add(Pemain(nama = "Zinedine Yazid Zidane", R.drawable.zidan, posisi = "Pelatih", tinggi = "1,85 m", tempatlahir = "Marseille (Prancis)", tgllahir = "23 Juni 1972"))
 
         binding.list.adapter = AdapterTeamBola(this, listPemain, object : AdapterTeamBola.OnClickListener {
             override fun detailData(item: Pemain?) {
-                TODO("Not yet implemented")
+                Dialog(this@MainActivity).apply {
+                    requestWindowFeature(Window.FEATURE_NO_TITLE)
+                    setCancelable(true)
+                    setContentView(R.layout.detail_data_pemain)
+
+                    val image = this.findViewById<ImageView>(R.id.image_pemain)
+                    val nama = this.findViewById<TextView>(R.id.txtNamaPemain)
+                    val posisi = this.findViewById<TextView>(R.id.txtPosisi)
+                    val tinggi = this.findViewById<TextView>(R.id.txtTinggi)
+                    val tempatlahir = this.findViewById<TextView>(R.id.txtTempatLahir)
+                    val tgllahir = this.findViewById<TextView>(R.id.txtTanggalLahir)
+                    val btn = this.findViewById<TextView>(R.id.btnClose)
+
+                    image.setImageResource(item?.foto ?:0)
+                    nama.text = "${item?.nama}"
+                    posisi.text = "${item?.posisi}"
+                    tinggi.text = "${item?.tinggi}"
+                    tempatlahir.text = "${item?.tempatlahir}"
+                    tgllahir.text = "${item?.tgllahir}"
+
+                    btn.setOnClickListener {
+                        this.dismiss()
+                    }
+                }.show()
             }
 
         })
